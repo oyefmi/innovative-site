98e30fe7fa5028c2f890a12cb66257293b756df2
Michael <michael@MAC21-ISI-01.local>
2024-01-16 17:51:35 -0500

    working to fix buges in Navbar functionality.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, TemplateRef } from '@angular/core';
+import { NgbOffcanvas, NgbOffcanvasConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
     isMenuCollapsed = true;
+
+    constructor(
+      config: NgbOffcanvasConfig,
+      private offcanvasService: NgbOffcanvas,
+    ) {
+      // customize default values of offcanvas used by this component tree
+      config.position = 'end';
+      config.backdropClass = 'bg-info';
+      config.keyboard = false;
+    }
+  
+    open(content: TemplateRef<any>) {
+      this.offcanvasService.open(content);
+    }
 }
